1bddf9117204bc94e11eecbefff87963f83c03ab
namal dilmith <namaldilmith2@gmail.com>
2024-09-24 11:36:20 +0530

    Create UI and Navigations in this system

---
+++ SuperMarket Management System/src/supermarket/management/system/SuperMarketManagementSystem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarket.management.system;
+
+/**
+ *
+ * @author user
+ */
+public class SuperMarketManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginForm login = new LoginForm();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+    
+}
